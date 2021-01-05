f44ee78aac308b8d57de4fd3dd1bc9bd910c816f
jinjinwen <274880230@qq.com>
2021-01-05 15:33:29 +0800

    优化

--- src/A.java
+++ src/A.java
@@ -1,6 +1,45 @@
-public class A {
-    String  vip="000eeeeeeeeerrrrrrr";
-    String B="222";
-    String C="222eeeeeeeeerrrrrr";
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+public class A {
+    String  name;
+
+    public String getName() {
+        System.out.println(1111);
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccout() {
+        return accout;
+    }
+
+    public void setAccout(String accout) {
+        this.accout = accout;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    String accout;
+    int age;
+
+    public A(String cc, String s, int i) {
+    }
+
+    public A() {
+
+    }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,14 +1,22 @@
 <%@ page import="java.util.Date" %>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ page import="A" %>
 <html>
   <head>
     <title>$Title$</title>
   </head>
   <body>
-  <% String str="1111";
-    out.println("qqq33333vvvvvvv");
-  %>
+  <jsp:useBean id="a" class="A"/>
+  <jsp:setProperty name="a" property="name" value="cc" />
+
   <%= new Date() %>
-  <% %>
+
+  cc:${"ssss"}
+  cc:${a.name}
+<%--  vv:${a.name}--%>
+
   </body>
 </html>
